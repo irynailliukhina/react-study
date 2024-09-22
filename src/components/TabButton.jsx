a1a0b13d2963735b5672066c3b248726f9e03118
Sirin <sirin@Sirins-MacBook-Pro.local>
2024-09-22 16:00:35 +0200

    initial project for React studies

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,8 @@
+export default function TabButton({ children, onSelect, isActive }) {
+
+    return (
+        <li>
+            <button className={isActive && 'active'} onClick={onSelect}>{children}</button>
+        </li>
+    )
+}
